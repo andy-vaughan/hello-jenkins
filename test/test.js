0865e4584f0cec1e7b576153ba9431a0af04d4ad
avaughan78 <avaughan78@gmail.com>
2016-10-10 15:13:20 +0100

    Baselined

--- test/test.js
+++ test/test.js
@@ -3,10 +3,6 @@ var app = require('../app.js');
  
 describe('GET /', function() {
   it('respond with hello Jenkins!', function(done) {
-<<<<<<< HEAD
-    request(app).get('/').expect('hello Jenkins!, done);
-=======
     request(app).get('/').expect('hello Jenkins!', done);
->>>>>>> 8cd79bda4579994beded1a9e020283fd307b0dba
   });
 });
